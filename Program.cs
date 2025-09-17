ea7a3fb9e1e7d634ceb8e14728303c93d3c42167
drago <drago@EM-LOPTAP>
2025-09-17 09:27:15 -0400

    Fixed password guessing game

--- Program.cs
+++ Program.cs
@@ -57,7 +57,6 @@
 
             guesses = 0;
 
-            //1
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("Guess the password.");
@@ -72,163 +71,161 @@
             {
                 guesses += 1;
                 Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guesses remaining.");
-            }
 
-            //2
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Guess the password.");
-            userGuess = Console.ReadLine();
-            Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Guess the password.");
+                userGuess = Console.ReadLine();
+                Console.WriteLine("");
 
-            if (userGuess.ToLower() == "password" && guesses < 5)
-            {
-                Console.WriteLine("Open Sesame!");
-            }
-            else
-            {
-                guesses += 1;
-                Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guesses remaining.");
-            }
+                if (userGuess.ToLower() == "password" && guesses < 5)
+                {
+                    Console.WriteLine("Open Sesame!");
+                }
+                else
+                {
+                    guesses += 1;
+                    Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guesses remaining.");
 
-            //3
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Guess the password.");
-            userGuess = Console.ReadLine();
-            Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("Guess the password.");
+                    userGuess = Console.ReadLine();
+                    Console.WriteLine("");
 
-            if (userGuess.ToLower() == "password" && guesses < 5)
-            {
-                Console.WriteLine("Open Sesame!");
-            }
-            else
-            {
-                guesses += 1;
-                Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guesses remaining.");
-            }
+                    if (userGuess.ToLower() == "password" && guesses < 5)
+                    {
+                        Console.WriteLine("Open Sesame!");
+                    }
+                    else
+                    {
+                        guesses += 1;
+                        Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guesses remaining.");
 
-            //4
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Guess the password.");
-            userGuess = Console.ReadLine();
-            Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("Guess the password.");
+                        userGuess = Console.ReadLine();
+                        Console.WriteLine("");
 
-            if (userGuess.ToLower() == "password" && guesses < 5)
-            {
-                Console.WriteLine("Open Sesame!");
-            }
-            else
-            {
-                guesses += 1;
-                Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guess remaining.");
-            }
+                        if (userGuess.ToLower() == "password" && guesses < 5)
+                        {
+                            Console.WriteLine("Open Sesame!");
+                        }
+                        else
+                        {
+                            guesses += 1;
+                            Console.WriteLine("Incorrect. You have " + (5 - guesses) + " guess remaining.");
 
-            //5
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Guess the password.");
-            userGuess = Console.ReadLine();
-            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("Guess the password.");
+                            userGuess = Console.ReadLine();
+                            Console.WriteLine("");
 
-            if (userGuess.ToLower() == "password" && guesses < 5)
-            {
-                Console.WriteLine("Open Sesame!");
-            }
-            else
-            {
-                Console.WriteLine("Access denied.");
-            }
+                            if (userGuess.ToLower() == "password" && guesses < 5)
+                            {
+                                Console.WriteLine("Open Sesame!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Access denied.");
+                            }
+                        }
+                    }
+                }
 
-            //Part 2
+                //Part 2
 
-            string pizzaTopping;
+                string pizzaTopping;
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("What is your favourite pizza topping?");
-            pizzaTopping = Console.ReadLine();
-            Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("What is your favourite pizza topping?");
+                pizzaTopping = Console.ReadLine();
+                Console.WriteLine("");
 
-            if (pizzaTopping.ToLower() == "ham" || pizzaTopping.ToLower() == "pineapple" || pizzaTopping.ToLower() == "bacon")
-            {
-                Console.WriteLine("Yummy!");
-            }
-            else
-            {
-                Console.WriteLine("Gross!");
-            }
+                if (pizzaTopping.ToLower() == "ham" || pizzaTopping.ToLower() == "pineapple" || pizzaTopping.ToLower() == "bacon")
+                {
+                    Console.WriteLine("Yummy!");
+                }
+                else
+                {
+                    Console.WriteLine("Gross!");
+                }
 
-            //Programming tasks
+                //Programming tasks
 
-            //task 1
+                //task 1
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.Write("Welcome to the bus station! Enter your age: ");
-            Int32.TryParse(Console.ReadLine(), out age);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Write("Welcome to the bus station! Enter your age: ");
+                Int32.TryParse(Console.ReadLine(), out age);
 
-            Console.WriteLine("");
-            Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
 
-            if (age <= 12 || age >= 60)
-            {
-                Console.WriteLine("Your ticket costs: $2.00");
-            }
-            else
-            {
-                Console.WriteLine("Your ticket costs: $3.50");
-            }
+                if (age <= 12 || age >= 60)
+                {
+                    Console.WriteLine("Your ticket costs: $2.00");
+                }
+                else
+                {
+                    Console.WriteLine("Your ticket costs: $3.50");
+                }
 
-            //task 2
+                //task 2
 
-            string favouriteAnimal;
+                string favouriteAnimal;
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.Write("What is your favourite animal?");
-            favouriteAnimal = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Write("What is your favourite animal?");
+                favouriteAnimal = Console.ReadLine();
 
-            Console.WriteLine("");
-            Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
 
-            if (favouriteAnimal.ToLower() == "dog" || favouriteAnimal.ToLower() == "horse")
-            {
-                Console.WriteLine("Me too!");
-            }
-            else
-            {
-                Console.WriteLine("To each their own.");
-            }
+                if (favouriteAnimal.ToLower() == "dog" || favouriteAnimal.ToLower() == "horse")
+                {
+                    Console.WriteLine("Me too!");
+                }
+                else
+                {
+                    Console.WriteLine("To each their own.");
+                }
 
-            //task 3
+                //task 3
 
-            double temperature;
-            string weather;
+                double temperature;
+                string weather;
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("What is the temperature outside?");
-            double.TryParse(Console.ReadLine(), out temperature);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("What is the temperature outside?");
+                double.TryParse(Console.ReadLine(), out temperature);
 
-            Console.WriteLine("");
-            Console.Write("What is the weather like?");
-            weather = Console.ReadLine();
+                Console.WriteLine("");
+                Console.Write("What is the weather like?");
+                weather = Console.ReadLine();
 
-            Console.WriteLine("");
+                Console.WriteLine("");
 
-            if (temperature >= 25 || weather.ToLower() == "sunny")
-            {
-                Console.WriteLine("Swim time!");
-            }
-            else
-            {
-                Console.WriteLine("Nap time!");
-            }
+                if (temperature >= 25 || weather.ToLower() == "sunny")
+                {
+                    Console.WriteLine("Swim time!");
+                }
+                else
+                {
+                    Console.WriteLine("Nap time!");
+                }
 
                 //
                 Console.ReadLine();
+            }
         }
     }
 }
+
+
